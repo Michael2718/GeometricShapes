5f3f0c194cabe77e8e8ea621665703aff85d2f2a
Michael2718 <isaevmihail007@gmail.com>
2022-06-01 21:22:24 +0000

    Add TODO tasks

--- src/AbstractShapeSubclasses/AbstractPolygonSubclasses/ConvexPolygon.cpp
+++ src/AbstractShapeSubclasses/AbstractPolygonSubclasses/ConvexPolygon.cpp
@@ -6,7 +6,8 @@ using std::invalid_argument;
 ConvexPolygon::ConvexPolygon(unsigned int id, const std::vector<Point>& points)
     : AbstractPolygon(id, points.size()), vertices(points) {
     if (vertices.size() < 3) throw invalid_argument("Invalid Polygon size.");
+    // TODO : Implement ConvexPolygon check
 }
 
-double ConvexPolygon::Perimeter() const {return 1;}
-double ConvexPolygon::Area() const {return 2;}
+double ConvexPolygon::Perimeter() const {return 1;} // TODO : Implement Perimeter()
+double ConvexPolygon::Area() const {return 2;} // TODO: Implement Area()
