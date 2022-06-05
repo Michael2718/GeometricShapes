b9d29211032c06bee8679ee1b45645625fedd125
Michael2718 <isaevmihail007@gmail.com>
2022-06-05 00:57:29 +0000

    Add basic Trapezoid class realization

--- src/AbstractShapeSubclasses/AbstractPolygonSubclasses/AbstractQuadrilateralSubclasses/Trapezoid.cpp
+++ src/AbstractShapeSubclasses/AbstractPolygonSubclasses/AbstractQuadrilateralSubclasses/Trapezoid.cpp
@@ -1,1 +1,29 @@
 #include "Trapezoid.h"
+#include <stdexcept>
+#include <cmath>
+
+using std::invalid_argument;
+
+Trapezoid::Trapezoid(unsigned int id, Point center, double a, double b, double height)
+    : AbstractQuadrilateral(id), center(center), a(a), b(b), height(height) {
+    if (a <= 0 || b <= 0 || height <= 0) throw invalid_argument("Invalid diagonals.");
+}
+
+void Trapezoid::SetA(double new_a) {
+    if (new_a <= 0) throw invalid_argument("Invalid new 'a' side.");
+    a = new_a;
+}
+
+void Trapezoid::SetB(double new_b) {
+    if (new_b <= 0) throw invalid_argument("Invalid new 'b' side.");
+    b = new_b;
+}
+
+void Trapezoid::SetHeight(double new_height) {
+    if (new_height <= 0) throw invalid_argument("Invalid new height.");
+    height = new_height;
+}
+
+double Trapezoid::Perimeter() const {return 0;}
+
+double Trapezoid::Area() const {return (a + b)/2*height;}
--- src/AbstractShapeSubclasses/AbstractPolygonSubclasses/AbstractQuadrilateralSubclasses/Trapezoid.h
+++ src/AbstractShapeSubclasses/AbstractPolygonSubclasses/AbstractQuadrilateralSubclasses/Trapezoid.h
@@ -2,9 +2,25 @@
 #define GEOMETRICSHAPES_TRAPEZOID_H
 
 #include "../AbstractQuadrilateral.h"
+#include "../../../Point.h"
 
 class Trapezoid : public AbstractQuadrilateral {
+private:
+    Point center; // Center is located at the middle of midline.
+    double a, b, height; // 'a' is top side parallel to bottom 'b' side.
+public:
+    Trapezoid(unsigned int id, Point center, double a, double b, double height);
 
+    double GetA() const {return a;}
+    double GetB() const {return b;}
+    double GetHeight() const {return height;}
+
+    void SetA(double new_a);
+    void SetB(double new_b);
+    void SetHeight(double new_height);
+    // TODO : Implement Perimeter() method
+    double Perimeter() const override;
+    double Area() const override;
 };
 
 
