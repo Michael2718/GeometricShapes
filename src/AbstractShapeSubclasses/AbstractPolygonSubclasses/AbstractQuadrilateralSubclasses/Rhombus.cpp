0a415c66ba38606d18deb545ecd8e16f7f4d229b
Michael2718 <isaevmihail007@gmail.com>
2022-06-05 00:08:28 +0000

    Add basic realization of Rhombus class

--- src/AbstractShapeSubclasses/AbstractPolygonSubclasses/AbstractQuadrilateralSubclasses/Rhombus.cpp
+++ src/AbstractShapeSubclasses/AbstractPolygonSubclasses/AbstractQuadrilateralSubclasses/Rhombus.cpp
@@ -1,1 +1,24 @@
 #include "Rhombus.h"
+#include <stdexcept>
+#include <cmath>
+
+using std::invalid_argument;
+
+Rhombus::Rhombus(unsigned int id, Point center, double d1, double d2)
+    : AbstractQuadrilateral(id), center(center), d1(d1), d2(d2) {
+    if (d1 <= 0 || d2 <= 0) throw invalid_argument("Invalid diagonals.");
+}
+
+void Rhombus::SetD1(double new_d1) {
+    if (new_d1 <= 0) throw invalid_argument("Invalid new first diagonal.");
+    d1 = new_d1;
+}
+
+void Rhombus::SetD2(double new_d2) {
+    if (new_d2 <= 0) throw invalid_argument("Invalid new second diagonal.");
+    d1 = new_d2;
+}
+
+double Rhombus::Perimeter() const {return 2*sqrt(d1*d1 + d2*d2);}
+
+double Rhombus::Area() const {return (d1*d2)/2;}
--- src/AbstractShapeSubclasses/AbstractPolygonSubclasses/AbstractQuadrilateralSubclasses/Rhombus.h
+++ src/AbstractShapeSubclasses/AbstractPolygonSubclasses/AbstractQuadrilateralSubclasses/Rhombus.h
@@ -2,9 +2,23 @@
 #define GEOMETRICSHAPES_RHOMBUS_H
 
 #include "../AbstractQuadrilateral.h"
+#include "../../../Point.h"
 
 class Rhombus : public AbstractQuadrilateral {
+private:
+    Point center;
+    double d1, d2; // d1 - diagonal parallel to OX axis; d2 - diagonal parallel to OY axis.
+public:
+    Rhombus(unsigned int id, Point center, double d1, double d2);
 
+    double GetD1() const {return d1;}
+    double GetD2() const {return d2;}
+
+    void SetD1(double new_d1);
+    void SetD2(double new_d2);
+
+    double Perimeter() const override;
+    double Area() const override;
 };
 
 
