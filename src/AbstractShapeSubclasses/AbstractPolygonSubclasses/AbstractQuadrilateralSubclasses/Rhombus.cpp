23278ad266cfe3778b5804a792ae73566f0b6ff7
Michael2718 <isaevmihail007@gmail.com>
2022-06-07 00:13:41 +0000

    Add Rhombus class constructor with three points parameters

--- src/AbstractShapeSubclasses/AbstractPolygonSubclasses/AbstractQuadrilateralSubclasses/Rhombus.cpp
+++ src/AbstractShapeSubclasses/AbstractPolygonSubclasses/AbstractQuadrilateralSubclasses/Rhombus.cpp
@@ -5,8 +5,21 @@
 using std::invalid_argument;
 
 Rhombus::Rhombus(unsigned int id, Point center, double d1, double d2)
-    : AbstractQuadrilateral(id), center(center), d1(d1), d2(d2) {
+    : AbstractQuadrilateral(id, Point(), Point(), Point(), Point()), center(center), d1(d1), d2(d2) {
     if (d1 <= 0 || d2 <= 0) throw invalid_argument("Invalid diagonals.");
+}
+
+Rhombus::Rhombus(unsigned int id, Point center, Point p1, Point p2)
+    : AbstractQuadrilateral(id, p1, p2,
+                            Point(2*center.X()-p1.X(), 2*center.Y()- p1.Y()),
+                            Point(2*center.X()-p2.X(), 2*center.Y()- p2.Y())),
+    center(center),
+    d1(sqrt(pow(2*(p1.X() - center.X()), 2) + pow(2*(p1.Y() - center.Y()), 2))),
+    d2(sqrt(pow(2*(p2.X() - center.X()), 2) + pow(2*(p2.Y() - center.Y()), 2))) {
+    if ((p1.X()-center.X())*(p2.X()-center.X()) + (p1.Y()-center.Y())*(p2.Y()-center.Y()) != 0) {
+        throw invalid_argument("Invalid points.");
+    }
+    if (p1.Y() < p2.Y()) throw invalid_argument("Invalid points.");
 }
 
 void Rhombus::SetD1(double new_d1) {
--- src/AbstractShapeSubclasses/AbstractPolygonSubclasses/AbstractQuadrilateralSubclasses/Rhombus.h
+++ src/AbstractShapeSubclasses/AbstractPolygonSubclasses/AbstractQuadrilateralSubclasses/Rhombus.h
@@ -10,6 +10,7 @@ private:
     double d1, d2; // d1 - diagonal parallel to OX axis; d2 - diagonal parallel to OY axis.
 public:
     Rhombus(unsigned int id, Point center, double d1, double d2);
+    Rhombus(unsigned int id, Point center, Point p1, Point p2);
 
     double GetD1() const {return d1;}
     double GetD2() const {return d2;}
