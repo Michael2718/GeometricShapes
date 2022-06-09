195972773d55f104f4e5b29ed2bc7e8e1e20db29
Michael2718 <isaevmihail007@gmail.com>
2022-06-09 01:02:29 +0000

    Add a... lot.. of.. methods in ConvexPolygon class

--- src/AbstractShapeSubclasses/AbstractPolygonSubclasses/ConvexPolygon.cpp
+++ src/AbstractShapeSubclasses/AbstractPolygonSubclasses/ConvexPolygon.cpp
@@ -1,14 +1,71 @@
 #include "ConvexPolygon.h"
 #include <stdexcept>
+#include <cmath>
 
-using std::invalid_argument;
+using std::invalid_argument, std::vector;
 
-ConvexPolygon::ConvexPolygon(unsigned int id, const std::vector<Point>& points)
-    : AbstractPolygon(id, points.size()), vertices(points) {
+bool isConvex(const vector<Point>& points) {
+    size_t n = points.size();
+    double x1, y1, x2, y2; // Coordinates of vectors (x1, y1) and (x2, y2)
+    bool positive = false, negative = false;
+    for (int i = 0; i < points.size(); i++) {
+        x1 = (points[(i+1)%n].X()-points[i%n].X());
+        y1 = (points[(i+1)%n].Y()-points[i%n].Y());
+        x2 = (points[(i+2)%n].X()-points[(i+1)%n].X());
+        y2 = (points[(i+2)%n].Y()-points[(i+1)%n].Y());
+        if(x1*y2 - y1*x2 > 0) {
+            positive = true;
+        } else if (x1*y2 - y1*x2 < 0) {
+            negative = true;
+        }
+    }
+    return !(positive && negative);
+}
+
+Point CalculateCenter(const vector<Point>& points) {
+    unsigned int n = points.size();
+    double center_x = 0, center_y = 0;
+    for (auto point: points) {center_x += point.X(); center_y += point.Y();}
+    return {center_x/n, center_y/n};
+}
+ConvexPolygon::ConvexPolygon(unsigned int id, const vector<Point>& points)
+    : AbstractPolygon(id, points.size()), vertices(points),
+    center(CalculateCenter(points)) {
     if (vertices.size() < 3) throw invalid_argument("Invalid Polygon size.");
-    // TODO : Implement ConvexPolygon check
+    for (int i = 0; i < points.size()-1; i++) {
+        for (int j = i+1; j < points.size(); j++) {
+            if (points[i] == points[j]) throw invalid_argument("Invalid Points.");
+        }
+    }
+    if (!isConvex(points)) throw invalid_argument("Invalid Points.");
+}
+
+void ConvexPolygon::Scale(double k) {
+    if (k <= 0) throw invalid_argument("Invalid scale factor k.");
+    vector<Point> new_vertices;
+    for (auto point: GetVertices()) {
+        new_vertices.emplace_back(k*point.X()+(1-k)*center.X(),
+                                k*point.Y()+(1-k)*center.Y());
+    }
+    SetPoints(new_vertices);
+}
+
+void ConvexPolygon::Rotate(double angle) {
+    vector<Point> new_vertices;
+    double r_angle = angle * (M_PI / 180.0);
+    double new_x, new_y;
+    for (auto point: GetVertices()) {
+        new_x = center.X() + (point.X()-center.X())*cos(r_angle) + (point.Y()-center.Y())*sin(r_angle);
+        new_y = center.Y() - (point.X()-center.X())*sin(r_angle) + (point.Y()-center.Y())*cos(r_angle);
+        new_vertices.emplace_back(new_x, new_y);
+    }
+    SetPoints(new_vertices);
 }
 
 double ConvexPolygon::Perimeter() const {return 1;} // TODO : Implement Perimeter()
 
 double ConvexPolygon::Area() const {return 2;} // TODO: Implement Area()
+
+void ConvexPolygon::SetPoints(const std::vector<Point> &new_vertices) {
+    vertices = new_vertices;
+}
--- src/AbstractShapeSubclasses/AbstractPolygonSubclasses/ConvexPolygon.h
+++ src/AbstractShapeSubclasses/AbstractPolygonSubclasses/ConvexPolygon.h
@@ -8,13 +8,20 @@
 class ConvexPolygon : public AbstractPolygon {
 private:
     std::vector<Point> vertices;
+    Point center;
 public:
     ConvexPolygon(unsigned int id, const std::vector<Point>& points);
 
     std::vector<Point> GetVertices() const {return vertices;}
+    Point GetCenter() const {return center;}
+
+    void Scale(double k) override;
+    void Rotate(double angle) override;
 
     double Perimeter() const override;
     double Area() const override;
+protected:
+    void SetPoints(const std::vector<Point>& new_vertices);
 };
 
 
