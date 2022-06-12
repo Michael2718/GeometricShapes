c43e758adacae25b09816e03025f1bac921ebf43
Michael2718 <isaevmihail007@gmail.com>
2022-06-12 21:44:40 +0000

    Add ComplexShape class basic realization

--- src/Shapes/ComplexShape.cpp
+++ src/Shapes/ComplexShape.cpp
@@ -1,1 +1,33 @@
 #include "ComplexShape.h"
+#include <stdexcept>
+#include "Point.h"
+
+using std::invalid_argument;
+
+ComplexShape::ComplexShape(unsigned int id, const std::vector<AbstractShape *> &shapes)
+    : AbstractShape(id), shapes(shapes), center({0, 0}) {
+    if (shapes.empty()) throw invalid_argument("Invalid number of shapes. Complex shape can't be empty.");
+    // TODO : Calculate center of ComplexShape
+}
+
+void ComplexShape::Scale(double k) {
+    // Translate all shapes
+    // Scale all shapes
+}
+
+void ComplexShape::Rotate(double angle) {
+    // Rotate all shapes relatively around center of ComplexShape
+}
+
+void ComplexShape::Translate(double dx, double dy) {
+    // Translate center
+    // Translate all shapes
+}
+
+double ComplexShape::Perimeter() const {
+    // Sum all perimeters
+}
+
+double ComplexShape::Area() const {
+    //Sum all areas
+}
--- src/Shapes/ComplexShape.h
+++ src/Shapes/ComplexShape.h
@@ -1,8 +1,27 @@
-//
-// Created by michael on 5/10/22.
-//
-
 #ifndef GEOMETRICSHAPES_COMPLEXSHAPE_H
 #define GEOMETRICSHAPES_COMPLEXSHAPE_H
+
+#include "AbstractShape.h"
+#include "Point.h"
+#include <vector>
+
+class ComplexShape : public AbstractShape {
+private:
+    std::vector<AbstractShape*> shapes;
+    Point center;
+public:
+    explicit ComplexShape(unsigned int id, const std::vector<AbstractShape*> &shapes);
+
+    std::vector<AbstractShape*> GetShapes() const {return shapes;}
+    Point GetCenter() const {return center;}
+
+    void Scale(double k) override;
+    void Rotate(double angle) override;
+    void Translate(double dx, double dy) override;
+
+    double Perimeter() const override;
+    double Area() const override;
+};
+
 
 #endif //GEOMETRICSHAPES_COMPLEXSHAPE_H
